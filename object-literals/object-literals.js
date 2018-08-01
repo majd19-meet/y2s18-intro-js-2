aff7c57d3551a3ea1b6ada6f50b3c75871b11627
majd19-meet <majd19@meet.mit.edu>
2018-08-01 12:15:01 +0300

    day3

--- object-literals/object-literals.js
+++ object-literals/object-literals.js
@@ -1,1 +1,16 @@
-// Create your 'me' object literal here!
+var me = {
+	firstName: "Majd",
+	lastName: "Nasrawi",
+	favoriteFoods: ["mansaf" , "pizza"],
+	age: 16,
+	picture: "https://goo.gl/images/28ePbT",
+}
+document.write(JSON.stringify(me))
+document.write("<br>")
+document.write(me.firstName);
+document.write("<br>")
+document.write(me.lastName);
+document.write("<br>")
+document.write(me.favoriteFoods);
+document.write("<br>")
+document.write(me.age);
